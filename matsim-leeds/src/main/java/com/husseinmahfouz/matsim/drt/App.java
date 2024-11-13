2540c6f2964bd9517ca4dcd2b9446f2a4f76100d
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-11-13 17:26:38 +0100

    add maven project

---
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/drt/App.java
@@ -0,0 +1,13 @@
+package com.husseinmahfouz.matsim.drt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
