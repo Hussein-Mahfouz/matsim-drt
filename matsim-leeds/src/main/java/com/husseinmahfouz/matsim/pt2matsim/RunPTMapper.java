40f3d12b4d5d10577b4bfc3ace5bb656f450e88b
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-11-14 17:03:21 +0100

    Map PT to road network

---
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/pt2matsim/RunPTMapper.java
@@ -0,0 +1,11 @@
+package com.husseinmahfouz.matsim.pt2matsim;
+
+import org.matsim.pt2matsim.run.PublicTransitMapper;
+
+public class RunPTMapper {
+
+    public static void main(String[] args) {
+        PublicTransitMapper.main(args);
+    }
+
+}
