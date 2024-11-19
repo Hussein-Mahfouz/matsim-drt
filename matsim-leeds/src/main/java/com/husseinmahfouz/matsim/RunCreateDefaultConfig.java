896164c398a4fd5d2c0664da61908920a048652b
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-11-19 13:23:00 +0100

    change directories and add main config class

---
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/RunCreateDefaultConfig.java
@@ -0,0 +1,11 @@
+package com.husseinmahfouz.matsim;
+
+import org.matsim.run.CreateFullConfig;
+
+public class RunCreateDefaultConfig {
+
+    public static void main(String[] args) {
+        CreateFullConfig.main(args);
+    }
+
+}
