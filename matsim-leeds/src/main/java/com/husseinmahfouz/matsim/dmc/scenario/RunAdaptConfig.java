2a1c77e6f96a06fdb474efa53dd82ff6f5b48b48
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-11-27 14:52:34 +0100

    initial idf code pre refactoring

---
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/dmc/scenario/RunAdaptConfig.java
@@ -0,0 +1,50 @@
+package org.eqasim.ile_de_france.scenario;
+
+import org.eqasim.core.components.config.ConfigAdapter;
+import org.eqasim.core.components.config.EqasimConfigGroup;
+import org.eqasim.ile_de_france.IDFConfigurator;
+import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup.VehiclesSource;
+import org.matsim.core.config.groups.VehiclesConfigGroup;
+
+public class RunAdaptConfig {
+	static public void main(String[] args) throws ConfigurationException {
+		ConfigAdapter.run(args, new IDFConfigurator(), RunAdaptConfig::adaptConfiguration);
+	}
+
+	static public void adaptConfiguration(Config config, String prefix) {
+		// Adjust eqasim config
+		EqasimConfigGroup eqasimConfig = EqasimConfigGroup.get(config);
+
+		eqasimConfig.setCostModel(TransportMode.car, IDFModeChoiceModule.CAR_COST_MODEL_NAME);
+		eqasimConfig.setCostModel(TransportMode.pt, IDFModeChoiceModule.PT_COST_MODEL_NAME);
+
+		eqasimConfig.setEstimator(TransportMode.car, IDFModeChoiceModule.CAR_ESTIMATOR_NAME);
+		eqasimConfig.setEstimator(TransportMode.bike, IDFModeChoiceModule.BIKE_ESTIMATOR_NAME);
+
+		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules()
+				.get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
+
+		dmcConfig.setModeAvailability(IDFModeChoiceModule.MODE_AVAILABILITY_NAME);
+
+		// Calibration results for 5%
+
+		if (eqasimConfig.getSampleSize() == 0.05) {
+			// Adjust flow and storage capacity
+			config.qsim().setFlowCapFactor(0.045);
+			config.qsim().setStorageCapFactor(0.045);
+		}
+
+		// Vehicles
+		QSimConfigGroup qsimConfig = config.qsim();
+		qsimConfig.setVehiclesSource(VehiclesSource.fromVehiclesData);
+
+		VehiclesConfigGroup vehiclesConfig = config.vehicles();
+		vehiclesConfig.setVehiclesFile(prefix + "vehicles.xml.gz");
+	}
+}
