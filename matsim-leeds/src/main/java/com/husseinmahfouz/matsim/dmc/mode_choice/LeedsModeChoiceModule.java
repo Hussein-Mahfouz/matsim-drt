dd3c9c4c2cbc07b8ecfdc27687c4639cce926efc
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2025-01-16 10:17:22 +0100

    add LeedsDrtCostModel

--- matsim-leeds/src/main/java/com/husseinmahfouz/matsim/dmc/mode_choice/LeedsModeChoiceModule.java
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/dmc/mode_choice/LeedsModeChoiceModule.java
@@ -11,12 +11,14 @@ import org.eqasim.core.simulation.mode_choice.parameters.ModeParameters;
 import org.eqasim.core.simulation.mode_choice.tour_finder.ActivityTourFinderWithExcludedActivities;
 import com.husseinmahfouz.matsim.dmc.mode_choice.costs.LeedsCarCostModel;
 import com.husseinmahfouz.matsim.dmc.mode_choice.costs.LeedsPtCostModel;
+import com.husseinmahfouz.matsim.dmc.mode_choice.costs.LeedsDrtCostModel;
 import com.husseinmahfouz.matsim.dmc.mode_choice.parameters.LeedsCostParameters;
 import com.husseinmahfouz.matsim.dmc.mode_choice.parameters.LeedsModeParameters;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators.LeedsBikeUtilityEstimator;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators.LeedsCarUtilityEstimator;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators.LeedsPtUtilityEstimator;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators.LeedsWalkUtilityEstimator;
+import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators.LeedsDrtUtilityEstimator;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.predictors.LeedsPersonPredictor;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.predictors.LeedsSpatialPredictor;
 import com.husseinmahfouz.matsim.dmc.mode_choice.utilities.predictors.LeedsPtPredictor;
@@ -36,11 +38,15 @@ public class LeedsModeChoiceModule extends AbstractEqasimExtension {
 
 	public static final String CAR_COST_MODEL_NAME = "LeedsCarCostModel";
 	public static final String PT_COST_MODEL_NAME = "LeedsPtCostModel";
+	// Add DRT
+	public static final String DRT_COST_MODEL_NAME = "LeedsDrtCostModel";
 
 	public static final String CAR_ESTIMATOR_NAME = "LeedsCarUtilityEstimator";
 	public static final String BIKE_ESTIMATOR_NAME = "LeedsBikeUtilityEstimator";
 	public static final String WALK_ESTIMATOR_NAME = "LeedsWalkUtilityEstimator";
 	public static final String PT_ESTIMATOR_NAME = "LeedsPtUtilityEstimator";
+	// Add DRT
+	public static final String DRT_ESTIMATOR_NAME = "LeedsDrtUtilityEstimator";
 
 	public static final String ISOLATED_OUTSIDE_TOUR_FINDER_NAME = "IsolatedOutsideTrips";
 
@@ -56,11 +62,16 @@ public class LeedsModeChoiceModule extends AbstractEqasimExtension {
 
 		bindCostModel(CAR_COST_MODEL_NAME).to(LeedsCarCostModel.class);
 		bindCostModel(PT_COST_MODEL_NAME).to(LeedsPtCostModel.class);
+		// Add DRT
+		bindCostModel(DRT_COST_MODEL_NAME).to(LeedsDrtCostModel.class);
 
 		bindUtilityEstimator(CAR_ESTIMATOR_NAME).to(LeedsCarUtilityEstimator.class);
 		bindUtilityEstimator(BIKE_ESTIMATOR_NAME).to(LeedsBikeUtilityEstimator.class);
 		bindUtilityEstimator(WALK_ESTIMATOR_NAME).to(LeedsWalkUtilityEstimator.class);
 		bindUtilityEstimator(PT_ESTIMATOR_NAME).to(LeedsPtUtilityEstimator.class);
+		// Add DRT
+		bindUtilityEstimator(DRT_ESTIMATOR_NAME).to(LeedsDrtUtilityEstimator.class);
+
 		bind(LeedsSpatialPredictor.class);
 		bind(LeedsPtPredictor.class);
 
