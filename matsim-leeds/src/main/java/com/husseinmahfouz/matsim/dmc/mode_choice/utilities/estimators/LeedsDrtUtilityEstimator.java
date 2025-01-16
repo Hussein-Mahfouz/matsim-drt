618e3216c0f4e34c99f98871a3f6b8f3a1c375b6
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2025-01-16 17:47:19 +0100

    update drt utility to match pt

--- matsim-leeds/src/main/java/com/husseinmahfouz/matsim/dmc/mode_choice/utilities/estimators/LeedsDrtUtilityEstimator.java
+++ matsim-leeds/src/main/java/com/husseinmahfouz/matsim/dmc/mode_choice/utilities/estimators/LeedsDrtUtilityEstimator.java
@@ -3,6 +3,7 @@ package com.husseinmahfouz.matsim.dmc.mode_choice.utilities.estimators;
 import java.util.List;
 
 import org.eqasim.core.simulation.modes.drt.mode_choice.utilities.estimators.DrtUtilityEstimator;
+import org.eqasim.core.simulation.mode_choice.utilities.variables.PtVariables;
 import org.eqasim.core.simulation.modes.drt.mode_choice.predictors.DrtPredictor;
 import org.eqasim.core.simulation.modes.drt.mode_choice.variables.DrtVariables;
 import com.husseinmahfouz.matsim.dmc.mode_choice.parameters.LeedsModeParameters;
@@ -43,6 +44,31 @@ public class LeedsDrtUtilityEstimator extends DrtUtilityEstimator {
         return utility;
     }
 
+    protected double estimateOutOfVehicleTimeUtility(DrtVariables variables) {
+        double utility = 0.0;
+        double lambda = parameters.leedsPT.lambdaOutofVehicleTime;
+
+        double OVT = variables.waitingTime_min + variables.accessEgressTime_min;
+
+        if (OVT > 1) {
+            utility += parameters.drt.betaWaitingTime_u_min
+                    * ((Math.pow(OVT, lambda) - 1) / lambda);
+        }
+
+        return utility;
+    }
+
+    @Override
+	protected double estimateMonetaryCostUtility(DrtVariables variables) {
+		double utility = 0.0;
+		double cost = variables.cost_MU;
+		// this avoids log(0) which is undefined
+		if (cost > 0) {
+			utility += parameters.betaCost_u_MU * Math.log(cost);
+		}
+		return utility;
+	}
+
 
 
     @Override
@@ -58,8 +84,9 @@ public class LeedsDrtUtilityEstimator extends DrtUtilityEstimator {
 
         utility += estimateConstantUtility();
         utility += estimateTravelTimeUtility(drtVariables);
-        utility += estimateWaitingTimeUtility(drtVariables);
-        utility += estimateAccessEgressTimeUtility(drtVariables);
+        // utility += estimateWaitingTimeUtility(drtVariables);
+        // utility += estimateAccessEgressTimeUtility(drtVariables);
+        utility += estimateOutOfVehicleTimeUtility(drtVariables);
         utility += estimateMonetaryCostUtility(drtVariables);
 
         return utility;
