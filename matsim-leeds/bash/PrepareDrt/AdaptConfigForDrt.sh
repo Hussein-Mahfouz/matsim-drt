032beef8c130fbf6678edabacc2f768aa0070837
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2025-03-19 19:24:18 +0000

    update preparation for different fleet sizes

--- matsim-leeds/bash/PrepareDrt/AdaptConfigForDrt.sh
+++ matsim-leeds/bash/PrepareDrt/AdaptConfigForDrt.sh
@@ -7,57 +7,64 @@
 
 # only set addLegTimeConstraint to false if you want to check demand out of the specified fleet operating times. You will have a lot of rejections
 
-# Define the fixed arguments
-inputConfigPath="src/main/resources/config_simulation_dmc.xml"
-outputConfigPath="src/main/resources/config_simulation_dmc_drt.xml"
-# outputConfigPath="src/main/resources/config_simulation_dmc_drt_all.xml"
+# Define the list of fleet sizes
+fleet_sizes=("50" "100" "200" "500" "1000")
 
-# Define the different sets of arguments
-declare -a argsArray=(
-    # Config that adds two fleets for the northwest and northeast service areas
-    "data/supply/drt/nw/fleets/drt_fleet_nw_50.xml,data/supply/drt/ne/fleets/drt_fleet_ne_50.xml \
-    drtNW,drtNE \
-    serviceAreaBased,serviceAreaBased \
-    true,true \
-    LeedsDrtCostModel,LeedsDrtCostModel \
-    LeedsDrtUtilityEstimator,LeedsDrtUtilityEstimator \
-    LeedsDrtModeAvailability"
-)
+# Loop through each fleet size
+for FLEET_SIZE in "${fleet_sizes[@]}"; do
+    # Define the fixed arguments
+    inputConfigPath="src/main/resources/config_simulation_dmc.xml"
 
-# declare -a argsArray=(
-#      # --- Config that adds a fleet for the entire study area
-#     "data/supply/drt/all/fleets/drt_fleet_all_50.xml \
-#     drt \
-#     door2door \
-#     true
-#     LeedsDrtCostModel \
-#     LeedsDrtUtilityEstimator \
-#     LeedsDrtModeAvailability"
-# )
+    # --- Define the different sets of arguments
+    outputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_${FLEET_SIZE}.xml"
+    declare -a argsArray=(
+        # Config that adds two fleets for the northwest and northeast service areas
+        "matsim-leeds/data/supply/drt/nw/fleets/drt_fleet_nw_${FLEET_SIZE}.xml,matsim-leeds/data/supply/drt/ne/fleets/drt_fleet_ne_${FLEET_SIZE}.xml \
+        drtNW,drtNE \
+        serviceAreaBased,serviceAreaBased \
+        true,true \
+        LeedsDrtCostModel,LeedsDrtCostModel \
+        LeedsDrtUtilityEstimator,LeedsDrtUtilityEstimator \
+        LeedsDrtModeAvailability"
+    )
 
-# Get the classpath for all dependencies and append target/classes (the former has core functionality e.g. core eqasim, the latter has the classes I wrote and compile)
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
-CLASSPATH=$(cat cp.txt):target/classes
+    # --- Define the different sets of arguments
+    # outputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_all_${FLEET_SIZE}.xml"
+    # declare -a argsArray=(
+    #     # Config that adds a fleet for the entire study area
+    #     "matsim-leeds/data/supply/drt/all/fleets/drt_fleet_all_${FLEET_SIZE}.xml \
+    #     drt \
+    #     door2door \
+    #     true \
+    #     LeedsDrtCostModel \
+    #     LeedsDrtUtilityEstimator \
+    #     LeedsDrtModeAvailability"
+    # )
 
-# Loop through the arguments and run the Java program
-for args in "${argsArray[@]}"; do
-    set -- $args
-    vehiclesPaths=$1
-    modeNames=$2
-    operationalSchemes=$3
-    addLegTimeConstraint=$4
-    drtCostModel=$5
-    drtUtilityEstimator=$6
-    modeAvailability=$7
+    # Get the classpath for all dependencies and append target/classes (the former has core functionality e.g. core eqasim, the latter has the classes I wrote and compile)
+    mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+    CLASSPATH=$(cat cp.txt):target/classes
 
-    java -cp $CLASSPATH com.husseinmahfouz.matsim.drt.RunAdaptConfigForDrt \
-        --input-config-path $inputConfigPath \
-        --output-config-path $outputConfigPath \
-        --vehicles-paths $vehiclesPaths \
-        --mode-names $modeNames \
-        --operational-schemes $operationalSchemes \
-        --add-leg-time-constraint $addLegTimeConstraint \
-        --cost-models $drtCostModel \
-        --estimators $drtUtilityEstimator \
-        --mode-availability $modeAvailability
+    # Loop through the arguments and run the Java program
+    for args in "${argsArray[@]}"; do
+        set -- $args
+        vehiclesPaths=$1
+        modeNames=$2
+        operationalSchemes=$3
+        addLegTimeConstraint=$4
+        drtCostModel=$5
+        drtUtilityEstimator=$6
+        modeAvailability=$7
+
+        java -cp $CLASSPATH com.husseinmahfouz.matsim.drt.RunAdaptConfigForDrt \
+            --input-config-path $inputConfigPath \
+            --output-config-path $outputConfigPath \
+            --vehicles-paths $vehiclesPaths \
+            --mode-names $modeNames \
+            --operational-schemes $operationalSchemes \
+            --add-leg-time-constraint $addLegTimeConstraint \
+            --cost-models $drtCostModel \
+            --estimators $drtUtilityEstimator \
+            --mode-availability $modeAvailability
+    done
 done
--- matsim-leeds/bash/PrepareDrt/AdaptConfigForFeederDrt.sh
+++ matsim-leeds/bash/PrepareDrt/AdaptConfigForFeederDrt.sh
@@ -5,54 +5,62 @@
 # Run this file from the following directory: matsim-leeds
 # If file is in matsim-leeds/bash, run ./bash/AdaptConfigForFeederDrt.sh
 
-# Define the fixed arguments
+# Define the list of fleet sizes
+fleet_sizes=("50" "100" "200" "500" "1000")
 
+# Loop through each fleet size
+for FLEET_SIZE in "${fleet_sizes[@]}"; do
 
-# Define the different sets of arguments (each row will produce a different config, so the outputCOnfigPath should be unique
-declare -a argsArray=(
-    "src/main/resources/config_simulation_dmc_drt.xml src/main/resources/config_simulation_dmc_drt_feeder.xml \
-    drtNW_feeder,drtNE_feeder \
-    pt \
-    drtNW,drtNE \
-    bus|rail,bus|rail \
-    LeedsDrtUtilityEstimator \
-    FeederDrtModeAvailabilityWrapper"
-)
+    # --- Define the different sets of arguments
+    inputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_${FLEET_SIZE}.xml"
+    outputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_${FLEET_SIZE}_feeder.xml"
+    declare -a argsArray=(
+        # Config that adds two fleets for the northwest and northeast service areas
+        "drtNW_feeder,drtNE_feeder \
+        pt \
+        drtNW,drtNE \
+        bus|rail,bus|rail \
+        LeedsDrtUtilityEstimator \
+        FeederDrtModeAvailabilityWrapper"
+    )
 
-# declare -a argsArray=(
-#     "src/main/resources/config_simulation_dmc_drt_all.xml src/main/resources/config_simulation_dmc_drt_all_feeder.xml \
-#     drt_feeder \
-#     pt \
-#     drt \
-#     bus|rail \
-#     LeedsDrtUtilityEstimator \
-#     FeederDrtModeAvailabilityWrapper"
-# )
+    # --- Define the different sets of arguments
+    # inputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_all_${FLEET_SIZE}.xml"
+    # outputConfigPath="src/main/resources/fleet_sizing/config_simulation_dmc_drt_all_${FLEET_SIZE}_feeder.xml"
+    # declare -a argsArray=(
+    #     # Config that adds a fleet for the entire study area
+    #     "drt_feeder \
+    #     pt \
+    #     drt \
+    #     bus|rail \
+    #     LeedsDrtUtilityEstimator \
+    #     LeedsDrtModeAvailability"
+    # )
 
-# Get the classpath for all dependencies and append target/classes (the former has core functionality e.g. core eqasim, the latter has the classes I wrote and compiled)
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
-CLASSPATH=$(cat cp.txt):target/classes
+    # Get the classpath for all dependencies and append target/classes (the former has core functionality e.g. core eqasim, the latter has the classes I wrote and compiled)
+    mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+    CLASSPATH=$(cat cp.txt):target/classes
 
-# Loop through the arguments and run the Java program
-for args in "${argsArray[@]}"; do
-    set -- $args
-    inputConfigPath=$1
-    outputConfigPath=$2
-    modeNames=$3
-    basePtModes=$4 # can remove as "pt" is default
-    baseDrtModes=$5
-    accessEgressTransitStopModes=$6
-   # accessEgressTransitStopIDs=$
-    estimators=$7
-    modeAvailability=$8
+    # Loop through the arguments and run the Java program
+    # Loop through the arguments and run the Java program
+    for args in "${argsArray[@]}"; do
+        set -- $args
+        modeNames=$1
+        basePtModes=$2 # can remove as "pt" is default
+        baseDrtModes=$3
+        accessEgressTransitStopModes=$4
+    # accessEgressTransitStopIDs=$
+        estimators=$5
+        modeAvailability=$6
 
-    java -cp $CLASSPATH org.eqasim.core.simulation.modes.feeder_drt.utils.AdaptConfigForFeederDrt \
-        --input-config-path $inputConfigPath \
-        --output-config-path $outputConfigPath \
-        --mode-names $modeNames \
-        --base-pt-modes $basePtModes \
-        --base-drt-modes $baseDrtModes \
-        --access-egress-transit-stop-modes $accessEgressTransitStopModes \
-        --estimators $estimators \
-        --mode-availability $modeAvailability
+        java -cp $CLASSPATH org.eqasim.core.simulation.modes.feeder_drt.utils.AdaptConfigForFeederDrt \
+            --input-config-path $inputConfigPath \
+            --output-config-path $outputConfigPath \
+            --mode-names $modeNames \
+            --base-pt-modes $basePtModes \
+            --base-drt-modes $baseDrtModes \
+            --access-egress-transit-stop-modes $accessEgressTransitStopModes \
+            --estimators $estimators \
+            --mode-availability $modeAvailability
+    done
 done
