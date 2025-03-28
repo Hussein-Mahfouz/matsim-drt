d0d6fcedaf8f08b82923ee20c15a76062151fcc6
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2025-03-28 19:38:06 +0000

    vehicle usage plots

--- matsim-leeds/R/code/mode_share.R
+++ matsim-leeds/R/code/mode_share.R
@@ -8,17 +8,28 @@ library(ggalluvial) # sankey
 # ---------- Read in the data
 
 # Input demand
-demand_original = arrow::read_parquet("../data/demand/legs_with_locations.parquet")
-# Output demand
-demand_matsim = read_delim("../scenarios/fleet_sizing/all/1000/sample_1.00/eqasim_trips.csv", delim =";")
-# demand_matsim = read_delim("../scenarios/fleet_sizing/zones/1000/sample_1.00/eqasim_trips.csv", delim =";")
+# demand_original = arrow::read_parquet("../data/demand/legs_with_locations.parquet")
+demand_original = read_delim("../scenarios/basic/sample_1.00/eqasim_trips.csv", delim =";")
 
+# Output demand
+scenario = "zones"
+# scenario = "all"
+fleet_size = 1000 # 20, 100, 200, 500, 1000
+
+demand_matsim = read_delim(paste0("../scenarios/fleet_sizing/", scenario, "/", fleet_size, "/sample_1.00/eqasim_trips.csv"), delim =";")
+
+# Add Trip distance
+demand_matsim <- demand_matsim %>%
+  mutate(euclidean_distance_bucket = cut(euclidean_distance,
+                           breaks = seq(0, ceiling(max(euclidean_distance, na.rm = TRUE) / 2500) * 2500, by = 2500),
+                           include.lowest = TRUE, right = FALSE))
 
 # ---------- Join input and output trips
 
 # Prepare data for joining (we join on person id and the equivalent of the trip sequence)
 demand_original = demand_original %>%
-  mutate(person_trip_id = seq - 1) %>%      # eqasim output is a person_trip_id which is seq starting from 0
+  # mutate(person_trip_id = seq - 1) %>%      # eqasim output is a person_trip_id which is seq starting from 0
+  rename(pid = person_id) %>%
   select(pid, person_trip_id, mode) %>%
   # rename_with(~ paste0("input_", .))
   rename_with(~ paste0("input_", .), .cols = c("mode"))
@@ -49,24 +60,22 @@ demand_compare_summary = demand_compare_summary %>%
   mutate(input_mode_trips_with_frac = glue::glue("{input_mode} ({input_mode_trips_frac} {'%'})")) # initial mode + initial mode share (%)
 
 
-#
-
 # Create a static Sankey-like diagram using ggplot and ggalluvial
 ggplot(data = demand_compare_summary, aes(axis1 = input_mode, axis2 = output_mode, y = trips)) +
   geom_alluvium(aes(fill = input_mode_trips_with_frac)) +
   geom_stratum() +
-  geom_text(stat = "stratum", aes(label = input_mode)) +
+  geom_text(stat = "stratum", aes(label = input_mode), size = 3) +
   # ggrepel::geom_text_repel(
   #   aes(label = input_mode),
   #   stat = "stratum", size = 4, direction = "y", nudge_x = -.5
   # ) +
   ggrepel::geom_text_repel(
     aes(label = output_mode),
-    stat = "stratum", size = 4, direction = "y", nudge_x = .25
+    stat = "stratum", size = 3, direction = "y", nudge_x = .35
   ) +
   scale_x_discrete(limits = c("Before", "After"), expand = c(0.15, 0.15)) +
   scale_y_continuous(labels = scales::label_comma()) +  # Format y-axis with commas
-  scale_fill_brewer(type = "qual", palette = "Set3") +
+  scale_fill_brewer(type = "qual", palette = "Set1") +
   theme_bw()  +
   theme(panel.border = element_blank()) +
   labs(
@@ -76,7 +85,47 @@ ggplot(data = demand_compare_summary, aes(axis1 = input_mode, axis2 = output_mod
     fill = "Initial mode \n(Initial mode share)"  # Change the legend title here
   )
 
-ggsave("sankey_all.png")
+ggsave(paste0("plots/mode_share/mode_share_sankey_", scenario, "_", fleet_size, ".png"))
+
+# ----- Same plot but combine all DRT (no distinction with feeder or specific fleets)
+demand_compare_summary_all <- demand_compare_summary %>%
+  mutate(output_mode_drt = if_else(str_detect(output_mode, regex("drt", ignore_case = TRUE)),
+                               "drt",
+                               output_mode)) %>%
+  # get total trips by DRT (we need one row per unique OD pair)
+  group_by(input_mode, output_mode_drt, input_mode_trips_with_frac) %>%
+  summarise(trips = sum(trips)) %>%
+  ungroup() %>%
+  filter(!is.na(output_mode_drt))
+
+
+ggplot(data = demand_compare_summary_all, aes(axis1 = input_mode, axis2 = output_mode_drt, y = trips)) +
+  geom_alluvium(aes(fill = input_mode_trips_with_frac)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = input_mode), size = 3) +
+  # ggrepel::geom_text_repel(
+  #   aes(label = input_mode),
+  #   stat = "stratum", size = 4, direction = "y", nudge_x = -.5
+  # ) +
+  ggrepel::geom_text_repel(
+    aes(label = output_mode_drt),
+    stat = "stratum", size = 3, direction = "y", nudge_x = .35
+  ) +
+  scale_x_discrete(limits = c("Before", "After"), expand = c(0.15, 0.15)) +
+  scale_y_continuous(labels = scales::label_comma()) +  # Format y-axis with commas
+  scale_fill_brewer(type = "qual", palette = "Set1") +
+  theme_bw()  +
+  theme(panel.border = element_blank()) +
+  labs(
+    title = "Shifts in Mode Share after introduction of DRT",
+    subtitle = "What modes did all trips transition from?",
+    y = "Number of trips",  # Change the y-axis label here
+    fill = "Initial mode \n(Initial mode share)"  # Change the legend title here
+  )
+
+ggsave(paste0("plots/mode_share/mode_share_sankey_", scenario, "_", fleet_size, "_sum_drt.png"))
+
+
 
 
 # Let's focus only on trips that came out as DRT
@@ -98,7 +147,7 @@ ggplot(data = demand_compare_summary_drt,
   # geom_text(stat = "stratum", aes(label = glue::glue("{input_mode} ({people})")))  +
   geom_text(stat = "stratum", aes(label = output_mode)) +
   scale_x_discrete(limits = c("Before", "After"), expand = c(0.15, 0.15)) +
-  scale_fill_brewer(type = "qual", palette = "Set3") +
+  scale_fill_brewer(type = "qual", palette = "Set1") +
   scale_y_continuous(labels = scales::label_comma()) +  # Format y-axis with commas
   theme_bw() +
   theme(panel.border = element_blank()) +
@@ -109,6 +158,6 @@ ggplot(data = demand_compare_summary_drt,
     fill = "Initial mode (% \nof trips that \nshifted to DRT)"  # Change the legend title here
   )
 
+ggsave(paste0("plots/mode_share/mode_share_sankey_", scenario, "_", fleet_size, "_drt.png"))
 
-ggsave("sankey_drt.png")
 
---
+++ matsim-leeds/R/code/operator_stats.R
@@ -0,0 +1,159 @@
+library(tidyverse)
+library(sf)
+library(tmap)
+
+# ----------- Specfy scenario
+
+# Output demand
+scenario = "zones"
+# scenario = "all"
+fleet_size = 200 # 20, 100, 200, 500, 1000
+drt_name = "drtNW"
+
+
+# ----------  Breakdown of vehicles by occupany (throughout the day) ---------- #
+
+drt_occupancy_time = read_delim(paste0("../scenarios/fleet_sizing/", scenario, "/", fleet_size, "/sample_1.00/output_occupancy_time_profiles_", drt_name, ".txt"), delim =";")
+
+
+drt_occupancy_time_l <- drt_occupancy_time %>%
+  pivot_longer(cols = -time, names_to = "Category", values_to = "Vehicles") %>%
+  group_by(time) %>%
+  mutate(Vehicles_Share = Vehicles / sum(Vehicles)) %>%
+  ungroup() %>%
+  filter(minute(time) %% 30 == 0) %>% # A value every 5 minutes is excessive
+  filter(as.numeric(time) < 24 * 60 * 60) %>% # Remove data points for time > 24
+  mutate(time = as.POSIXct(time, format = "%H:%M:%S"))
+
+drt_occupancy_time_l$Category <- forcats::fct_relevel(drt_occupancy_time_l$Category, "STAY", "RELOCATE",
+                                                      "0 pax", "1 pax", "2 pax", "3 pax", "4 pax")
+
+# --- Line plot
+ggplot(drt_occupancy_time_l #%>%
+         #filter(!(Category %in% c("STAY", "RELOCATE")))
+       ,
+       aes(x = time, y = Vehicles_Share, color = Category)) +
+  geom_line() +
+  scale_x_datetime(date_breaks = "4 hour",    # Adjust to show more labels every hour
+                   date_labels = "%H:%M",
+                   minor_breaks = "1 hour") +    # Show only hour and minute
+  labs(title = "Vehicle occupancy by time of day",
+       x = "",
+       y = "Proportion of Vehicles",
+       color = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# --- Area plot
+ggplot(drt_occupancy_time_l # %>%
+        # filter(!(Category %in% c("STAY", "RELOCATE")))
+       , aes(x = time, y = Vehicles_Share, fill = Category)) +
+  geom_area(position = "stack") +
+  scale_x_datetime(date_breaks = "4 hour",  # Adjust to show more labels every hour
+                   date_labels = "%H:%M",
+                   minor_breaks = "1 hour") +  # Show only hour and minute
+  labs(title = "Vehicle occupancy by time of day",
+       x = "",
+       y = "Proportion of Vehicles",
+       fill = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# ----------- DRT VKM travelled vs Passenger KM ---------- #
+
+# --- DRT vehicle KMs
+drt_vkm = read_delim(paste0("../scenarios/fleet_sizing/", scenario, "/", fleet_size, "/sample_1.00/eqasim_drt_vehicle_movements.csv"), delim =";")
+
+# times as hour of day
+drt_vkm_time = drt_vkm %>%
+  mutate(departure_time_h = round(departure_time / 3600),
+         arrival_time_h = round(arrival_time / 3600))
+
+# total distance travlled by hour of day
+drt_vkm_time = drt_vkm_time %>%
+  group_by(arrival_time_h, operator_id) %>%
+  summarise(distance_km = sum(distance) / 1000) %>%
+  mutate(metric = "vkm") %>%
+  ungroup() %>%
+  group_by(operator_id) %>%
+  mutate(distance_km_cum = cumsum(distance_km)) %>% # cumulative distance
+  ungroup()
+
+
+
+# --- DRT Passenger KMs
+drt_pkm = read_delim(paste0("../scenarios/fleet_sizing/", scenario, "/", fleet_size, "/sample_1.00/eqasim_drt_passenger_rides.csv"), delim =";")
+
+
+drt_pkm_time = drt_pkm %>%
+  mutate(departure_time_h = round(departure_time / 3600),
+         arrival_time_h = round(arrival_time / 3600))
+
+drt_pkm_time = drt_pkm_time %>%
+  group_by(arrival_time_h, operator_id) %>%
+  summarise(distance_km = sum(distance) / 1000) %>%
+  mutate(metric = "pkm") %>%
+  ungroup() %>%
+  group_by(operator_id) %>%
+  mutate(distance_km_cum = cumsum(distance_km)) %>% # cumulative distance
+  ungroup()
+
+# --- Plot
+
+# --- Combine vkm with pkm
+drt_km_time = drt_pkm_time %>%
+  bind_rows(drt_vkm_time)
+
+# --- Plot pkm and vkm every hour
+ggplot(drt_km_time, aes(x = arrival_time_h, y = distance_km, color = metric)) +
+  geom_line() +
+  labs(title = "Vehicle and passenger km throughout the day",
+       x = "",
+       y = "Distance (km)",
+       color = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(. ~ operator_id) +
+  scale_x_continuous(breaks = seq(0, 24, by = 1),
+                     labels = ifelse(seq(0, 24, by = 1) %% 4 == 0,
+                                     paste0(seq(0, 24, by = 1), ":00"),
+                                     ""))
+
+# --- Plot CUMULATIVE pkm and vkm every hour
+ggplot(drt_km_time, aes(x = arrival_time_h, y = distance_km_cum, color = metric)) +
+  geom_line() +
+  labs(title = "Cumulative vehicle and passenger km throughout the day",
+       x = "",
+       y = "Distance (km)",
+       color = "") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(. ~ operator_id) +
+  scale_x_continuous(breaks = seq(0, 24, by = 1),
+                     labels = ifelse(seq(0, 24, by = 1) %% 4 == 0,
+                                     paste0(seq(0, 24, by = 1), ":00"),
+                                     ""))
+
+
+# ------ Distance travelled by occupancy
+
+drt_vkm_occupancy = drt_vkm %>%
+  group_by(operator_id, number_of_passengers) %>%
+  summarise(distance_km = sum(distance / 1000)) %>%
+  ungroup()
+
+
+ggplot(drt_vkm_occupancy, aes(x = factor(number_of_passengers), y = distance_km, fill = factor(number_of_passengers))) +
+  geom_col() +
+  facet_wrap(~ operator_id) +  #
+  labs(title = "Total distance travelled by passenger load",
+       x = "Number of Passengers",
+       y = "Total Distance (KM)",
+       fill = "Number of Passengers") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none")
+
+
